a4fff1043bc54b809398a32617a3f942c96e9090
Kaelyn Sung <sungks@uci.edu>
2025-10-25 20:16:09 -0700

    Finished the first part of part 1

--- js/about.js
+++ js/about.js
@@ -9,6 +9,21 @@ function parseTweets(runkeeper_tweets) {
 		return new Tweet(tweet.text, tweet.created_at);
 	});
 	
+	//processing the earliest and latest times
+	if(tweet_array.length > 0){
+		const toDate = (tw) =>
+		tw.time instanceof Date ? tw.time : new Date(tw.time || tw.creeated_at);
+
+		const times = tweet_array.map(toDate);
+		const earliest = new Date(Math.min(...times.map(d => d.getTime())));
+		const latest   = new Date(Math.max(...times.map(d => d.getTime())));
+		
+		const fmt = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+
+		//update the spans 
+		document.getElementById('firstDate').innerText = earliest.toLocaleDateString(undefined, fmt);
+		document.getElementById('lastDate').innerText  = latest.toLocaleDateString(undefined, fmt);
+	}
 	//This line modifies the DOM, searching for the tag with the numberTweets ID and updating the text.
 	//It works correctly, your task is to update the text of the other tags in the HTML file!
 	document.getElementById('numberTweets').innerText = tweet_array.length;	
